a0eb8212983aee04d2cafc49cd20059cef59a645
FernandoSamayoa <fredysamayoa5@gmail.com>
2022-12-21 15:55:54 -0600

    lecture 5 code

--- cypress/integration/examples/Test2.js
+++ cypress/integration/examples/Test2.js
@@ -1,21 +1,15 @@
 //cypress - Spec
 /// <references types="Cypress"" />
 
-describe('My first Test Suite', function()
+describe('My second Test Suite', function()
 {
-it('My first case', function(){
+it('My Second case', function(){
         
 cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
 cy.get('.search-keyword').type('ca');
 cy.wait(2000);
-cy.get('.product').should('have.length',5); //we have 1 product that is invisible
-//Looking in the ENTIRE page
-cy.get('.product:visible').should('have.length',4);
 //Parent Child chaining
 cy.get('.products').as('productlocator') //aliasing the product get to optimize the future calls
-cy.get('@productlocator').find('.product').should('have.length', 4);
-cy.get('@productlocator').find('.product').eq(2).contains('ADD TO CART').click()
-
 cy.get('@productlocator')
     .find('.product')
     .each(($el, index, $list) => {
@@ -25,16 +19,11 @@ cy.get('@productlocator')
         }
     })
 
-//Assertion if logo value is fine
-cy.get('.brand').should('have.text', 'GREENKART')    
-//Print in log
-cy.get('.brand').then(function(arg){
-    cy.log(arg.text())
-})
-
-
-
-
+    cy.get('.cart-icon > img').click()
+    cy.contains('PROCEED TO CHECKOUT').click()
+    cy.wait(2000);
+    //cy.get(':nth-child(14)').click()
+    cy.contains('Place Order').click()
 
 })
 })
